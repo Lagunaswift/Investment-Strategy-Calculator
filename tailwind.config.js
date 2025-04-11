832ea4455a06ab61c7736356cbc889eca0f1d1ea
Lagunaswift <jamesswift1988@gmail.com>
2025-04-11 18:23:34 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    // Add other paths if necessary
+  ],
+  theme: {
+    extend: {
+      // Add custom theme settings here if needed
+      // e.g., colors, spacing, etc.
+    },
+  },
+  plugins: [],
+};
